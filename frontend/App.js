645881f3f6181e014298a42d9eecfdb0a6d17378
Shevchenko <shevchenkoartur745@gmail.com>
2022-05-31 18:53:34 +0300

    login/logout template

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,38 @@
 import 'regenerator-runtime/runtime';
 import React from 'react';
 import './assets/css/global.css';
-import getConfig from './assets/js/near/config';
 import Navbar from "./components/Navbar";
+import {Box, Typography} from "@mui/material";
+import Button from "@mui/material/Button";
+import {login} from "./assets/js/near/utils";
 
 const App = () => {
     return (
-        <Navbar />
+        <>
+            <Navbar/>
+            <Box>
+                {window.accountId
+                    ?
+                    <Box mt={3}>
+                        <Typography align='center'>Meta Data</Typography>
+                        <Typography align='center'>Send Token Out</Typography>
+                        <Typography align='center'>Active keys</Typography>
+                    </Box>
+                    :
+                    <Box mt={3}>
+                        <Typography align='center'>Please, login, otherwise app will not work!</Typography>
+                        <Box display='flex'>
+                            <Button
+                                variant='contained'
+                                style={{margin: '16px auto'}}
+                                onClick={login}
+                            >
+                                Login
+                            </Button>
+                        </Box>
+                    </Box>}
+            </Box>
+        </>
     );
 }
 
