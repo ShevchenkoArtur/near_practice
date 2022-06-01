b6d1fe1d4925c337ed6968b02459b8b80cc85509
Shevchenko <shevchenkoartur745@gmail.com>
2022-06-01 11:16:36 +0300

    display metadata

--- frontend/components/Metadata.js
+++ frontend/components/Metadata.js
@@ -1,20 +1,27 @@
 import React, {useEffect, useState} from 'react';
+import {Box, Typography} from "@mui/material";
 
 const Metadata = () => {
-    const [metadata, setMetadata] = useState();
+    const [metadata, setMetadata] = useState({});
 
     useEffect(() => {
         (async () => {
             const data = await window.account.state();
-            console.log(data);
             setMetadata(data);
         })()
     }, []);
 
     return (
-        <div>
-            Metadata
-        </div>
+        <Box ml={3}>
+            <Typography variant='h3'>Meta Data</Typography>
+            <Typography>Amount - {metadata?.amount}</Typography>
+            <Typography>Block hash - {metadata?.block_hash}</Typography>
+            <Typography>Block height - {metadata?.block_height}</Typography>
+            <Typography>Code hash - {metadata?.code_hash}</Typography>
+            <Typography>Locked - {metadata?.locked}</Typography>
+            <Typography>Storage paid at - {metadata?.storage_paid_at}</Typography>
+            <Typography>Storage usage - {metadata?.storage_usage}</Typography>
+        </Box>
     );
 };
 
