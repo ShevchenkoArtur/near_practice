0e1b3653a0dac8d3b7f5961ef23e6ed36715fd93
Shevchenko <shevchenkoartur745@gmail.com>
2022-06-02 15:50:23 +0300

    setup tests, add create method

--- contract/assembly/__tests__/index.spec.ts
+++ contract/assembly/__tests__/index.spec.ts
@@ -1,3 +1,14 @@
-describe('contract methods', () => {
+import { create } from "../index";
+import { Todo, todos } from "../model";
 
+describe("contract methods", () => {
+    it("creates a todo", () => {
+        // call the create method
+        const todo = create("Drink water");
+
+        // lookup in the PersistentUnorderedMap for our todo
+        // expect the persisted todo to equal the todo returned
+        // by the create method above.
+        expect(todos.getSome(todo.id)).toStrictEqual(todo);
+    });
 });
--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -1,13 +1,7 @@
 import { PersistentUnorderedMap, math } from "near-sdk-as";
 
-// Think of this PersistentUnorderedMap like a database table.
-// We'll use this to persist and retrieve data.
 export const todos = new PersistentUnorderedMap<u32, Todo>("todos");
 
-// Think of this like a model class in something like mongoose or
-// sequelize. It defines the shape or schema for our data. It will
-// also contain static methods to read and write data from and to
-// the todos PersistentUnorderedMap.
 @nearBindgen
 export class Todo {
     id: u32;
@@ -18,5 +12,18 @@ export class Todo {
         this.id = math.hash32<string>(task);
         this.task = task;
         this.done = false;
+    }
+
+    static insert(task: string): Todo {
+        // create a new Todo
+        const todo = new Todo(task);
+
+        // add the todo to the PersistentUnorderedMap
+        // where the key is the todo's id and the value
+        // is the todo itself. Think of this like an
+        // INSERT statement in SQL.
+        todos.set(todo.id, todo);
+
+        return todo;
     }
 }
